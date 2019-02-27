757735163c73bc4f3e41551d6211ebb3c6ece80e
Jaime Junior Cabrera Párraga <jaimecabparr@gmail.com>
2019-02-27 00:30:26 -0400

    Tercer enlace

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NoticiasComponent } from './noticias/noticias.component';
 import { VideosComponent } from './videos/videos.component';
+import { EstadisticasComponent } from './estadisticas/estadisticas.component';
 
 
 const routes: Routes = [
   {path: 'noticias', component: NoticiasComponent},
-  {path: 'videos', component: VideosComponent}
+  {path: 'videos', component: VideosComponent},
+  {path: 'estadisticas', component: EstadisticasComponent}
 ];
 
 @NgModule({
