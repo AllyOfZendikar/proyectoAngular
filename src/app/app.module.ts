68b4026cfdbd0c4fd77f07bafc4ba97299403121
Jaime Junior Cabrera Párraga <jaimecabparr@gmail.com>
2019-03-05 18:57:11 -0400

    Utilizando clases de Bootstrap adecuadamente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import { ImageComponent } from './image/image.component';
 import { NoticiasComponent } from './noticias/noticias.component';
 import { VideosComponent } from './videos/videos.component';
 import { EstadisticasComponent } from './estadisticas/estadisticas.component';
@@ -14,7 +13,6 @@ import { EstadisticasComponent } from './estadisticas/estadisticas.component';
   declarations: [
     AppComponent,
     NavComponent,
-    ImageComponent,
     NoticiasComponent,
     VideosComponent,
     EstadisticasComponent
