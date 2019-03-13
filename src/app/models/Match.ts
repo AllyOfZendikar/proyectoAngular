52410002f0f9c24cf095c92a55113484a371cf5b
Jaime Junior Cabrera Párraga <jaimecabparr@gmail.com>
2019-03-13 15:39:00 -0400

    Creación de interfaz Match

---
+++ src/app/models/Match.ts
@@ -0,0 +1,23 @@
+export interface Match{
+    id: string;
+    year: string;
+    round: string;
+    local: string;
+    visitor: string;
+    team1: string;
+    team2: string;
+    local_abbr: string;
+    visitor_abbr: string;
+    competition_name: string;
+    total_rounds: string;
+    local_shield: string;
+    visitor_shield: string;
+    date: string;
+    hour: string;
+    minute: string;
+    local_goals: string;
+    visitor_goals: string;
+    result: string;
+    status: number;
+    winner: string;
+}
